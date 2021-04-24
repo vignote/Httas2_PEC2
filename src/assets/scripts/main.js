8d65d1306bd8a06d5812d4baa1ff9f79c18f0143
Alejandro Rodriguez Vignote <alejandro.vignote@gmail.com>
2021-04-24 22:32:31 +0200

    Mi primer Commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+import * as bootstrap from "bootstrap";
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
